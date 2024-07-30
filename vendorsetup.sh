88c368e1d05a841a2d3c44f8b36c0256ffc6217b
picasso09 <119201284+picasso09@users.noreply.github.com>
2024-07-30 15:21:40 +0000

    earth: sync local change lazy write:/
    Signed-off-by: picasso09 <119201284+picasso09@users.noreply.github.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,8 @@
 echo 'Device setup'
-git clone --depth=1 https://github.com/picasso09/vendor_xiaomi_earth -b lineage21 vendor/xiaomi/earth
+git clone  https://github.com/picasso09/vendor_xiaomi_earth -b lineage21 vendor/xiaomi/earth
 git clone --depth=1 https://github.com/picasso09/kernel-earth-S-OSS -b lineage-21 kernel/realme/earth
 git clone --depth=1 https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr device/mediatek/sepolicy_vndr
+git clone --depth=1 https://github.com/LineageOS/android_hardware_mediatek hardware/mediatek
+git clone --depth=1 https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
+
 echo 'Done go to cook'
